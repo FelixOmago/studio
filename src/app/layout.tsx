007543914db8c09b2bd8b2ec9662c07042e1bb20
josue <Edianojosue25@gmail.com>
2025-10-07 03:06:54 +0000

    esta dando algum erro, nao consigo salvar no git hub, aparentemente temo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,6 @@ const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 export const metadata: Metadata = {
   title: "Squara: CryptoTracker",
   description: "Rastreie os valores de Bitcoin, Solana e Ethereum em tempo real.",
-  manifest: "/manifest.webmanifest",
   openGraph: {
     title: "Squara: CryptoTracker",
     description: "Rastreie os valores de Bitcoin, Solana e Ethereum em tempo real.",
